a3b65a474a6793842e2556988ae80581c85bbb80
Chandragithubuk <shrutishikha18@gmail.com>
2020-12-28 14:28:28 +0000

    Changes made -userid check

--- src/main/java/com/qa/pages/ContactPage.java
+++ src/main/java/com/qa/pages/ContactPage.java
@@ -2,6 +2,9 @@ package com.qa.pages;
 
 public class ContactPage {
     public void testcp(){
+
+        System.out.println("Test New page");
+
         System.out.println("Test New page");
     }
 }
